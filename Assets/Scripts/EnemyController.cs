bd43b614f7f128292e0320f12803e4147e1c472d
Brock Kennedy <brock97k@gmail.com>
2023-02-25 17:08:17 -0400

    Updated the logic for enemy scripts seeing the player

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,7 +6,6 @@ public class EnemyController : MonoBehaviour
 {
     [Min(0)] public float range = 10;
     public Transform player;
-    public bool seePlayer = false;
     public float speed = 1;
 
 
@@ -15,6 +14,7 @@ public class EnemyController : MonoBehaviour
     float fallMultiplier = 3f;
     float lowJumpMultiplier = 8f;
     public bool canJump = true;
+    public Vector3 lastPlayerSeenPos;
 
     private float nextActionTime = 5f;
     public float period = 5f;
@@ -26,27 +26,28 @@ public class EnemyController : MonoBehaviour
     }
 
     void Update() {
-        //inRange = (Vector2.Distance(player.position, transform.position) <= range);
-        RaycastTowardsPlayer(player);
-        if (seePlayer) {
-            Walk(((player.position.x - transform.position.x) >= 0) ? 1 : -1);
+        //inRange = (Vector2.Distance(player.position, transform.position) <= range
+        if (seePlayer(player)) {
+            lastPlayerSeenPos = player.position;
         }
-
-        if (Physics2D.Raycast(transform.position, Vector2.down, 0.55f, LayerMask.GetMask("Floor"))) {
-            canJump = true;
+        if (lastPlayerSeenPos != Vector3.zero) {
+            Walk(((lastPlayerSeenPos.x - transform.position.x) >= 0) ? 1 : -1);
         }
-        if ((Time.time > nextActionTime) && canJump) {
-            canJump = false;
-            nextActionTime = Time.time + period;
-            Jump();
-        }
+        //if (Physics2D.Raycast(transform.position, Vector2.down, 0.55f, LayerMask.GetMask("Floor"))) {
+        //    canJump = true;
+        //}
+        //if ((Time.time > nextActionTime) && canJump) {
+        //    canJump = false;
+        //    nextActionTime = Time.time + period;
+        //    Jump();
+        //}
     }
-    void RaycastTowardsPlayer(Transform player) {
+    bool seePlayer(Transform player) {
         Vector2 raycastDir = player.position - transform.position;
         var floorHit = Physics2D.Raycast(transform.position, raycastDir, range, 64, 0);
         var playerHit = Physics2D.Raycast(transform.position, raycastDir, range, 256, 0);
         // Not hitting a wall but hitting a player
-        seePlayer = (floorHit.distance == 0 && playerHit.distance != 0);
+        return (floorHit.distance == 0 && playerHit.distance != 0);
     }
     private void Walk(float xDir) {
         rb.velocity = new Vector2(xDir * speed, rb.velocity.y);
